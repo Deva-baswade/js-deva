f015f1a0ab65b1a424a86b09af6a72800b4e1879
Deva-baswade <baswadedeva45@gmail.com>
2025-02-17 17:12:36 +0000

    datatypes of javasript

---
+++ deva/data_types.js
@@ -0,0 +1,17 @@
+"use strict";//this is for our code alway in newer form
+//code have more readibility
+console.log("deva"); console.log("pavan");//this should avoid in real life that meke bad impression
+
+let name = "deva";//string datatype
+let number = 12345;//number datatype this rabge of number about like 2 to power 53 to 53 something
+let isRight = true;//boolean datatype
+//for big integer we use bitint;
+                                     
+//null for =>standlone value   when we tyof null is given object
+//undefined => not given
+//symbol=>unique use for uniqueness in code
+                                                     //this above are primitive type now non-primitive
+//object
+console.log(typeof"deva");
+
+
