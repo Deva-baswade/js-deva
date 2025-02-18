b6a9beb4140a1b49dc409b19a945bbe31a36f278
Deva-baswade <baswadedeva45@gmail.com>
2025-02-18 11:27:36 +0000

    this string and its method

---
+++ deva/String.js
@@ -0,0 +1,35 @@
+const name = "devanand";
+const repoCount = 50;
+//console.log(name+repoCount);//dont use just like this
+console.log(`the name is ${name} and the repoCount is ${repoCount}`);
+
+const gameName = "baswadedeva";
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);//this denote str is object
+
+//str method
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(0));
+// console.log(gameName.indexOf('b'));
+
+// let newString = gameName.substring(-1,2);
+// console.log(newString);
+
+// const sliceString = gameName.slice(-1,4);
+// console.log(sliceString);
+
+
+//for rmoving the space using the trim property
+// const str1 = "  shivajirao. ";
+// console.log(str1);//this print the   shivajirao 
+// console.log(str1.trim());// this print the shivajirao
+
+const nameof = "devanand is pro devloper as well as software engineer";
+// console.log(nameof.replace('de','pa'))//replace of string
+
+//*******question asking********
+// console.log(nameof.includes('deva'))****this return boolean value
+//spilt into the words
+console.log(nameof.split(','));
