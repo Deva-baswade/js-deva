f392ac1f7a17a7ee91e6795e44db423e2c24fd0a
Deva-baswade <baswadedeva45@gmail.com>
2025-02-18 01:39:19 +0000

    opration on number and string

---
+++ deva/opration.js
@@ -0,0 +1,37 @@
+//opartion on the string and the number
+let number = 45;
+let negofnumber = -number;
+console.log(negofnumber);
+//for power
+console.log(2**3);//where 3 is power of 2
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%3);//for the remainder of this one
+
+let str1 = " baswade";
+let str2 = "devanand";
+let str3 = str2+str1;
+console.log(str3);
+
+
+console.log(1+"2");//=>12
+console.log("1"+2);//=>12
+console.log("1"+2+2)//=> 122 js convert all into string
+console.log(1+2+"2");//=> 32 add first then add with third str
+
+//=>+true give the true but true+ give error
+//=>+"" give false value or 0
+
+let gameCounter=100;
+gameCounter++//this give 101
+++gameCounter//this is give 101
+// study the perfix the sufix on mdn
+
+
+
+
+
+
+
