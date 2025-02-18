e850672987485b2320963ae4097d2754f22ea1d5
Deva-baswade <baswadedeva45@gmail.com>
2025-02-18 05:53:48 +0000

    this the various datatype example and detail explanation

---
+++ deva/data-type02.js
@@ -0,0 +1,32 @@
+//datatypes catagory into two part primitive and non-primitive
+//primtive
+const score = 45;
+const ID = 456;
+const isLoged = false;
+const  outside_temp = null;
+let userEmail;//this undefined type
+const id = Symbol('12345');
+console.log(id);
+let bignumber = 12345678910n;
+console.log(bignumber);
+//7 types:string,number,boolean,null,undefined,symbol,BigInt
+
+
+//reference(non-primitive) : array,objects,Functions
+//type of variable define in lang called static dont then dyanamically
+//so js is dynamically type
+
+let arr = ["devnand","pavan","ram"];
+let myobj = {
+    name:devanand,
+    age:19
+};
+let myfunction = function(){
+    console.log("hello deva");
+    
+}
+//for checking the type use typrof
+
+
+
+
