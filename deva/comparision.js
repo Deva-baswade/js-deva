1ee6154d9ac69b2a6ab3aee9aee756e3739d77d7
Deva-baswade <baswadedeva45@gmail.com>
2025-02-18 04:48:48 +0000

    this comprision

---
+++ deva/comparision.js
@@ -0,0 +1,34 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);//true str convert into in the number
+
+// console.log(null>0);
+// console.log(null>=0);//this true beasuce here null treat as 0 and then comparision
+// console.log(null==0);//this suprise me this result
+
+// console.log(undefined==0);
+// console.log(undefined>=0);
+// console.log(undefined!=0);
+
+console.log("2"==2);//=>true this only compare value asa it is
+
+console.log("2"===2);//=>false this strict check means also check the datatype
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
