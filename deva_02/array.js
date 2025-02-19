77149c450d2fa7c8e9d1c6b9786e89526b99dddc
Deva-baswade <baswadedeva45@gmail.com>
2025-02-19 03:12:34 +0000

    this is part one of the array

---
+++ deva_02/array.js
@@ -0,0 +1,39 @@
+////non-primitive data-tyepe;
+////array
+
+// const number = [0,1,2,3,4,5,6,7,8,9,10];
+// const name = ['deva','pavan','ram','shivaji','baswade'];
+// const defineArray = new Array(1,2,3,4,5);
+// //for accesing
+// console.log(name[1],number[1],defineArray);//pavan,1,[ 1, 2, 3, 4, 5 ]
+
+////method of array
+
+//let myArray = [1,2,3,4,5];
+// myArray.push(6);//=>[ 6 ] this add in last
+// myArray.unshift(9)//=>[ 9, 1, 2, 3, 4, 5 ] this add in front
+// myArray.shift()//this method delate the first value [ 2, 3, 4, 5 ]
+// myArray.pop();//=> delete last value
+
+// let array = [1,2,3,4,5];
+// let array2 = [5,4,3,2];
+// console.log(array.concat(array2));//=>[1,2,3,4,5,5,4,3,2]
+// console.log(array.includes(5));//=>true
+// console.log(array.indexOf(5));//=>4
+// let newarray = array.join()//this datatype convert into string copy all value
+
+////*******this difference bateween slice and spilce***
+
+// let array =[1,2,3,4,5];
+// console.log(array);
+// const array1 = array.slice(1,3);
+// console.log("a ",array1);
+// const array2 = array.splice(1,3);
+// console.log("b ",array2);
+
+
+
+
+
+
+
