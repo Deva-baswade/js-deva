70341d08cba74e65de6c90f04668eba3afa79193
Baswade_Devanand_Shivajirao <baswadedeva45@gmail.com>
2025-02-22 15:18:12 +0530

    the third file of obj

---
+++ deva_02/object03.js
@@ -0,0 +1,9 @@
+////the destuctring the object 
+const user ={
+    coursename:"javasript",
+    coursetrcher:"devanand",
+    courseprice:999
+}
+
+const {coursename:name} = user
+console.log(name);
