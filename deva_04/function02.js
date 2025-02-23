b2d4201768451ba4366031a654094b9395e056d1
Baswade_Devanand_Shivajirao <baswadedeva45@gmail.com>
2025-02-23 12:09:01 +0530

    Update function02.js

--- deva_04/function02.js
+++ deva_04/function02.js
@@ -1,22 +1,22 @@
 //add cart in web
-// function calculatecartPrice(...num1){//... this the spread or the rest oprstor
-//     return num1;
-// }
-// console.log(calculatecartPrice(2,3,5))
+function calculatecartPrice(...num1){//... this the spread or the rest oprstor
+    return num1;
+}
+console.log(calculatecartPrice(2,3,5))
 
-// const user ={
-//     username:"name",
-//     userId:12345
-// }
-// function myobj(anyobject){
-//     console.log(anyobject.username);
+const user ={
+    username:"name",
+    userId:12345
+}
+function myobj(anyobject){
+    console.log(anyobject.username);
     
-// }
-// myobj(user)//this the normal method
-// myobj({
-//     username:"name",
-//     userid:123
-// })//this also can valid
+}
+myobj(user)//this the normal method
+myobj({
+    username:"name",
+    userid:123
+})//this also can valid
 
 const mynewarray = [100,200,300]
 
