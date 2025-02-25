40d485bf758ae5839452ae25819c49a48aa59f2a
Baswade_Devanand_Shivajirao <baswadedeva45@gmail.com>
2025-02-25 12:09:39 +0530

    the object iteration

---
+++ deva_04/object_itration.js
@@ -0,0 +1,20 @@
+// const myobj ={
+//     name:"deva",
+//     number:12345,
+//     age:12
+// }
+
+// for (const key in myobj) {
+//     console.log(key,myobj[key]);
+    
+//     // name deva
+//     // number 12345
+//     // age 12
+// }
+
+const array = ['deva','ram','pavan']
+for(const i in array){
+console.log(i);
+console.log(array[i]);
+
+}
--- deva_04/three_loop.js
+++ deva_04/three_loop.js
@@ -1,10 +1,10 @@
-//this loop based on the array
+// this loop based on the array
 
-//for  of loop
+// for  of loop
 
 const number =[1,2,3,4,5];
 for (const element of number) {
-    //console.log(element);
+    console.log(element);
     
     
 }
@@ -12,29 +12,98 @@ for (const element of number) {
 const greeting = "devaannd";
 
 for(const i of greeting){
-    //console.log(i);
+    console.log(i);
     
 }
 
 //maps   
 
-// const map = new Map();
-// map.set('ind',"india");//this not store the duplicate
-// map.set('usa',"united state of america")
-// map.set('fra',"france")
+const map = new Map();
+map.set('ind',"india");//this not store the duplicate
+map.set('usa',"united state of america")
+map.set('fra',"france")
 
-// //  console.,log(map)
+//  console.,log(map)
 
-// for(const [i,j] of map){
-//     console.log(i,j);
+for(const [i,j] of map){
+    console.log(i,j);
+}
+
+// const myobject = {
+//     number:12345,
+//     name:"deva",
+//     age:19
+// }
+// for(const i of myobject){
+//     console.log(i);//this give the error
+    
 // }
 
-const myobject = {
-    number:12345,
+
+
+//this the for in loop
+const myobj ={
     name:"deva",
-    age:19
+    number:12345,
+    age:12
 }
-for(const i of myobject){
-    console.log(i);
+
+for (const key in myobj) {
+    console.log(key,myobj[key]);
+    
+    // name deva
+    // number 12345
+    // age 12
+}
+
+const array = ['deva','ram','pavan']
+for(const i in array){
+console.log(i);
+console.log(array[i]);
+
+}
+
+
+
+
+const coding =['deva','pavan','ram']
+coding.forEach(function(val){
+    console.log(val);
+    
+})
+coding.forEach((item) =>{
+    console.log(item);
+    
+})
+
+
+function printMe(item){
+    console.log(item);
     
 }
+coding.forEach(printMe)
+
+coding.forEach((item,index,array)=>{
+    console.log(item,index,array);
+    
+})
+
+//in array in object
+
+const userdata =[
+    {
+        username:"deva",
+        userage:"19"
+    },
+    {
+        username:"ram",
+        userage:"19"
+    },
+    {
+        username:"pavan",
+        userage:"19"
+    }
+]
+userdata.forEach((item)=>{
+        console.log(item.username);
+})
