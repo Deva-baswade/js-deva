74e3df34b47a06d7dc6cd314db0af8344f08d36d
Baswade_Devanand_Shivajirao <baswadedeva45@gmail.com>
2025-02-25 09:20:39 +0530

    the for loop

---
+++ deva_04/for_loop.js
@@ -0,0 +1,14 @@
+//loop cld the iterator
+
+for(let i=0;i<=10;i++){
+    console.log(i);
+    
+}
+for(let i=0;i<=10;i++){
+    if(i==5){
+        console.log("brst number");
+        
+    }
+    console.log("not best number");
+    
+}
