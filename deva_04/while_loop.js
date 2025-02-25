4347010abf10284e1587dcecebc03b214e89fdda
Baswade_Devanand_Shivajirao <baswadedeva45@gmail.com>
2025-02-25 10:51:20 +0530

    this file is while loop in js

---
+++ deva_04/while_loop.js
@@ -0,0 +1,23 @@
+// let i =1;
+// while(i<=10){
+//     console.log(i);
+//     i++
+    
+// };
+
+// let array=['deva','pavan','ram']
+// let j=0;
+// while(j<array.length){
+//     console.log(array[j]);
+//     j++
+    
+// }
+
+//do while 
+
+
+let number=21;
+do{
+    console.log(number);
+    number++;
+}while(number<=20)
