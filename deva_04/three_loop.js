7f4ba35d4618bc080fe1cd1777e1b646a50b7896
Baswade_Devanand_Shivajirao <baswadedeva45@gmail.com>
2025-02-25 11:06:41 +0530

    this the special loop file

---
+++ deva_04/three_loop.js
@@ -0,0 +1,40 @@
+//this loop based on the array
+
+//for  of loop
+
+const number =[1,2,3,4,5];
+for (const element of number) {
+    //console.log(element);
+    
+    
+}
+
+const greeting = "devaannd";
+
+for(const i of greeting){
+    //console.log(i);
+    
+}
+
+//maps   
+
+// const map = new Map();
+// map.set('ind',"india");//this not store the duplicate
+// map.set('usa',"united state of america")
+// map.set('fra',"france")
+
+// //  console.,log(map)
+
+// for(const [i,j] of map){
+//     console.log(i,j);
+// }
+
+const myobject = {
+    number:12345,
+    name:"deva",
+    age:19
+}
+for(const i of myobject){
+    console.log(i);
+    
+}
