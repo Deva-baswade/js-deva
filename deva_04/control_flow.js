d5e3a784630a7008cdda94b5e819612aed7c12bf
Baswade_Devanand_Shivajirao <baswadedeva45@gmail.com>
2025-02-25 09:07:15 +0530

    we study the if and the switch case

---
+++ deva_04/control_flow.js
@@ -0,0 +1,23 @@
+//if
+//oprator use in the is and else
+//<,>,>=,<=,!=,==,=== strict check ckeck data type also !==
+
+// let age=18;
+
+// if(age<=20){
+//     console.log("able");
+    
+// }else{
+//     console.log("not able");
+    
+// }
+
+const islogged = true;
+const debit = true;
+if(islogged && debit){
+    console.log("yes");
+    
+}else{
+    console.log("no");
+    
+}
