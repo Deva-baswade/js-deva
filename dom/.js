5faa5f41266df8071e7be6215e104c935b64f169
Baswade_Devanand_Shivajirao <baswadedeva45@gmail.com>
2025-02-28 05:58:21 +0530

    result not show at the remote

---
+++ dom/.js
@@ -0,0 +1,10 @@
+console.log('deva');
+//const var1 = documnet.getElementById('id)
+//document.getElementByclass('classname)
+//documnet.querySelctor()
+//var.style.backgound-color='green'
+//documnet.queryselctoeall('li')
+//const var2=documnet.queryselctorall('li')
+//var2[0].style.color='green'
+//var5.foreach(function(li){
+// li.style.color='blue'})
