939ec25fecd588f2fcdb4bc46755c1e7f7106da9
Baswade_Devanand_Shivajirao <baswadedeva45@gmail.com>
2025-03-05 08:19:30 +0530

    remove the comment part

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -1,34 +1,34 @@
-// const power5 = function(number){
-//     return number*5;
-// }
-// power5.Power=5;
-// console.log(power5(2));
-// console.log(power5.Power);
-// console.log(power5.prototype);
+const power5 = function(number){
+    return number*5;
+}
+power5.Power=5;
+console.log(power5(2));
+console.log(power5.Power);
+console.log(power5.prototype);
 
-// function createUser(username, score){
-//     this.username = username;
-//     this.score = score;
-// }
+function createUser(username, score){
+    this.username = username;
+    this.score = score;
+}
 
-// createUser.prototype.increment = function(){
-//     this.score++;
-// }
+createUser.prototype.increment = function(){
+    this.score++;
+}
 
-// createUser.prototype.print = function(){
-//     console.log(this.username, this.score);
-// }
+createUser.prototype.print = function(){
+    console.log(this.username, this.score);
+}
 
-// const deva = new createUser('deva', 2);
-// const ram = new createUser('ram', 3);
+const deva = new createUser('deva', 2);
+const ram = new createUser('ram', 3);
 
-// deva.increment();
-// deva.print();
-// ram.increment();
-// ram.print();
+deva.increment();
+deva.print();
+ram.increment();
+ram.print();
 
-// let myName = 'deva     ';//we have another property myname.trim().length
-// console.log(myName.truelength);//this for the treu length
+let myName = 'deva     ';//we have another property myname.trim().length
+console.log(myName.truelength);//this for the treu length
 
 
 let devaBhai = {
